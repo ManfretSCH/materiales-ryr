9af90d4036083a58a39aa48140ee2d39908a8eb2
Maiquel Schoter <manfretmichael11@gmail.com>
2025-09-06 16:16:51 -0300

    Agregar nueva base en vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/materiales-ryr/', // <--- ¡ESTO ES LO IMPORTANTE!
   plugins: [react()],
-  base: '/ferreteria/' // <- agrega el nombre de tu repo aquí
-})
+});
