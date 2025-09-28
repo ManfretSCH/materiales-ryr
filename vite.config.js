c828068833ebd186bc4e70f8779b6cc9c5a79a5d
Maiquel Schoter <manfretmichael11@gmail.com>
2025-09-28 10:51:31 -0300

    fix de manego de ruta

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/materiales-ryr/', // <--- ¡ESTO ES LO IMPORTANTE!
+  base: '/materiales-ryr/',
   plugins: [react()],
 });
